386519997c5d30f2bc877af9bea48ba04c73e895
tojo-rmo <tojo.rmo@welovetojo.com>
2026-08-18 17:52:15 +0900

    Update common.js

--- common.js
+++ common.js
@@ -36,7 +36,6 @@ const FOOTER_HTML = `
       <div class="foot-nav">
         <div class="foot-col">
           <h5>Contents</h5>
-          <a href="index.html">HOME</a>
           <a href="about.html">RMOとは</a>
           <a href="solutions.html">3つの目的</a>
           <a href="challenges.html">地域の課題</a>
@@ -46,7 +45,7 @@ const FOOTER_HTML = `
           <h5>Links</h5>
           <a href="https://sites.google.com/view/okamotoeinou/" target="_blank" rel="noopener">㈱岡本営農互助会</a>
           <a href="https://www.welovetojo.com/" target="_blank" rel="noopener">We Love シン東条</a>
-          <a href="https://www.instagram.com/kato.agri.japan/" target="_blank" rel="noopener">Instagram</a>
+          <a href="https://www.city.kato.lg.jp/" target="_blank" rel="noopener">加東市役所</a>
         </div>
       </div>
     </div>
